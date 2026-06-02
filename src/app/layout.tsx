572753b83a4ea66aee2e43675f5b1d1e523e825d
itsshiva78 <2022002786.shiva@ug.sharda.ac.in>
2026-06-02 12:20:22 +0530

    chore: explicitly hardcode Google AdSense meta tag, script snippet, and ads.txt

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -130,6 +130,8 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${inter.variable} ${spaceGrotesk.variable}`} suppressHydrationWarning>
       <head>
+        <meta name="google-adsense-account" content="ca-pub-7893678534155164" />
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7893678534155164" crossOrigin="anonymous"></script>
         <script
           suppressHydrationWarning
           dangerouslySetInnerHTML={{
